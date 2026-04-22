7805f75bd57809cd1740aa1da6219722560d09d7
‎G. <91469450+sys-arch@users.noreply.github.com>
2026-04-22 23:42:42 +0200

    Website rework

--- func/oauth-callback.js
+++ func/oauth-callback.js
@@ -6,7 +6,16 @@ import { createJwt } from "./helpers/jwt-helpers.js";
 function parseCookies(str) {
     return str
         .split(';')
-        .map(v => v.split('='))
+        .map(v => v.trim())
+        .filter(Boolean)
+        .map(v => {
+            const index = v.indexOf('=');
+            if (index === -1) {
+                return [v, ''];
+            }
+
+            return [v.slice(0, index), v.slice(index + 1)];
+        })
         .reduce((acc, v) => {
             acc[v[0]] = v[1];
             return acc;
@@ -14,7 +23,7 @@ function parseCookies(str) {
 }
 
 function verifyCsrf(event) {
-    if (event.queryStringParameters.state !== undefined && event.headers.cookie !== undefined) {
+    if (event.queryStringParameters?.state !== undefined && event.headers?.cookie !== undefined) {
         const cookies = parseCookies(event.headers.cookie);
         if (cookies["__Secure-CSRFState"] !== undefined) {
             return cookies["__Secure-CSRFState"] === event.queryStringParameters.state;
@@ -31,29 +40,70 @@ export async function handler(event, context) {
         };
     }
 
-    if (verifyCsrf(event)) {
-        if (event.queryStringParameters.code !== undefined) {
-            const result = await fetch("https://discord.com/api/oauth2/token", {
-                method: "POST",
-                body: new URLSearchParams({
-                    client_id: process.env.DISCORD_CLIENT_ID,
-                    client_secret: process.env.DISCORD_CLIENT_SECRET,
-                    grant_type: "authorization_code",
-                    code: event.queryStringParameters.code,
-                    redirect_uri: new URL(event.path, DEPLOY_PRIME_URL),
-                    scope: "identify"
-                })
-            });
-    
-            const data = await result.json();
-    
-            if (!result.ok) {
-                console.log(data);
-                throw new Error("Failed to get user access token");
-            }
-    
-            const user = await getUserInfo(data.access_token);
-            if (isBlocked(user.id)) {
+    if (!verifyCsrf(event)) {
+        return {
+            statusCode: 403
+        };
+    }
+
+    if (event.queryStringParameters?.code === undefined) {
+        return {
+            statusCode: 400
+        };
+    }
+
+    const result = await fetch("https://discord.com/api/oauth2/token", {
+        method: "POST",
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            grant_type: "authorization_code",
+            code: event.queryStringParameters.code,
+            redirect_uri: new URL(event.path, DEPLOY_PRIME_URL),
+            scope: "identify email"
+        })
+    });
+
+    const data = await result.json();
+
+    if (!result.ok) {
+        console.log(data);
+        throw new Error("Failed to get user access token");
+    }
+
+    const user = await getUserInfo(data.access_token);
+
+    if (isBlocked(user.id)) {
+        return {
+            statusCode: 303,
+            headers: {
+                "Location": `/error?msg=${encodeURIComponent("You cannot submit ban appeals with this Discord account.")}`,
+            },
+        };
+    }
+
+    const userPublic = {
+        id: user.id,
+        avatar: user.avatar,
+        username: user.username,
+        discriminator: user.discriminator,
+        email: user.email || null
+    };
+
+    if (process.env.GUILD_ID && !process.env.SKIP_BAN_CHECK && !process.env.DISCORD_WEBHOOK_URL) {
+        const ban = await getBan(user.id, process.env.GUILD_ID, process.env.DISCORD_BOT_TOKEN);
+
+        if (ban === null) {
+            return {
+                statusCode: 303,
+                headers: {
+                    "Location": "/error"
+                }
+            };
+        }
+
+        if (ban.reason) {
+            if (ban.reason.includes("/p") || ban.reason.includes("/perm")) {
                 return {
                     statusCode: 303,
                     headers: {
@@ -62,56 +112,18 @@ export async function handler(event, context) {
                 };
             }
 
-            const userPublic = {
-                id: user.id,
-                avatar: user.avatar,
-                username: user.username,
-                discriminator: user.discriminator
-            };
-    
-            if (process.env.GUILD_ID && !process.env.SKIP_BAN_CHECK && !process.env.DISCORD_WEBHOOK_URL) {
-                const ban = await getBan(user.id, process.env.GUILD_ID, process.env.DISCORD_BOT_TOKEN);
-                if (ban === null) {
-                    return {
-                        statusCode: 303,
-                        headers: {
-                            "Location": "/error"
-                        }
-                    };
-                }
-
-                if (ban.reason) {
-                    if (ban.reason.includes("/p") || ban.reason.includes("/perm")) {
-                        return {
-                            statusCode: 303,
-                            headers: {
-                                "Location": `/error?msg=${encodeURIComponent("You cannot submit ban appeals with this Discord account.")}`,
-                            },
-                        };
-                    }
-
-                    if (!process.env.ALWAYS_HIDE_BAN_REASON) {
-                        if(!ban.reason.includes("/h") && !ban.reason.includes("/hide")) {
-                            userPublic.reason = ban.reason;
-                        }
-                    }
+            if (!process.env.ALWAYS_HIDE_BAN_REASON) {
+                if (!ban.reason.includes("/h") && !ban.reason.includes("/hide")) {
+                    userPublic.reason = ban.reason;
                 }
             }
-
-            return {
-                statusCode: 303,
-                headers: {
-                    "Location": `/form?token=${encodeURIComponent(createJwt(userPublic, data.expires_in))}`
-                }
-            };
-        } else {
-            return {
-                statusCode: 400
-            };
         }
-    } else {
-        return {
-            statusCode: 403
-        };
     }
+
+    return {
+        statusCode: 303,
+        headers: {
+            "Location": `/form?token=${encodeURIComponent(createJwt(userPublic, data.expires_in))}`
+        }
+    };
 }
--- func/unban.js
+++ func/unban.js
@@ -9,25 +9,35 @@ export async function handler(event, context) {
     }
 
     if (event.queryStringParameters.token !== undefined) {
-        const unbanInfo = decodeJwt(event.queryStringParameters.token);
-        if (unbanInfo.userId !== undefined) {
-            try {
-                await unbanUser(unbanInfo.userId, process.env.GUILD_ID, process.env.DISCORD_BOT_TOKEN);
-                
-                return {
-                    statusCode: 303,
-                    headers: {
-                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
-                    }
-                };
-            } catch (e) {
-                return {
-                    statusCode: 303,
-                    headers: {
-                        "Location": `/error?msg=${encodeURIComponent("Failed to unban user\nPlease manually unban")}`
-                    }
-                };
+        try {
+            const unbanInfo = decodeJwt(event.queryStringParameters.token);
+
+            if (unbanInfo.userId !== undefined) {
+                try {
+                    await unbanUser(unbanInfo.userId, process.env.GUILD_ID, process.env.DISCORD_BOT_TOKEN);
+
+                    return {
+                        statusCode: 303,
+                        headers: {
+                            "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
+                        }
+                    };
+                } catch (e) {
+                    return {
+                        statusCode: 303,
+                        headers: {
+                            "Location": `/error?msg=${encodeURIComponent("Failed to unban user\nPlease manually unban")}`
+                        }
+                    };
+                }
             }
+        } catch (e) {
+            return {
+                statusCode: 303,
+                headers: {
+                    "Location": `/error?msg=${encodeURIComponent("Invalid or expired token")}`
+                }
+            };
         }
     }
 
